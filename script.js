9c0c1c7d0181fa8d70a625a7df7688248fa3d9c3
dishag07 <dishag422@gmail.com>
2024-05-22 14:23:07 -0400

    done w java

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
+let LIST, id;
 
 const options = {weekday : "long", month:"short", day:"numeric"}
 const today = new Date();
@@ -16,9 +17,14 @@ const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 function addToDo(toDo, id, done, trash){
+
+        if(trash) { return; }
+        const DONE = done ? CHECK : UNCHECK;
+        const LINE = done ? LINE_THROUGH : "";
+
     const item = ` <li class="item">
-    <i class="fa fa-circle-thin co" job="complete" id="${id}"></i> 
-    < p class="text">${toDo}</p>
+    <i class="fa ${DONE} co" job="complete" id="${id}"></i> 
+    < p class="text ${LINE}">${toDo}</p>
     <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
     </li>
     `;
@@ -26,13 +32,21 @@ function addToDo(toDo, id, done, trash){
     list.insertAdjacentHTML(position, item);
 }
 
-addToDo("drink coffee");
+
 
 document.addEventListener("keyup", function(even){
     if(event.keyCode == 13){
         const toDo = input.value; 
         if(toDo){
-            addToDo(toDo);
+            addToDo(toDo, id, false, false);
+            
+            LIST.push({
+                name : toDo,
+                id : id,
+                done : false,
+                trash : false
+            });
+                id++;
         }
     input.value = "";
     }
